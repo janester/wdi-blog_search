9175c0437039ce7e52045a57856c3dfd206170e1
Jane Sternbach <janesternbach@gmail.com>
2013-07-26 11:27:13 -0700

    uncommented searching methods in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,18 +3,18 @@ class Article < ActiveRecord::Base
   has_many :comments, :order => "created_at ASC"
   attr_accessible :content, :name, :published_at, :author
 
-  # searchable do
-  #   text :name, :boost => 5
-  #   text :content
+  searchable do
+    text :name, :boost => 5
+    text :content
 
-  #   text :comments do
-  #     comments.map {|c| c.content}
-  #   end
+    text :comments do
+      comments.map {|c| c.content}
+    end
 
-  #   text :author do
-  #     author.name
-  #   end
+    text :author do
+      author.name
+    end
 
-  #   date :published_at
-  # end
+    date :published_at
+  end
 end
